8fd72368d254aa03d59fb84b1acd9c6db0f6e270
Harshal-0901 <harshalpresswala09@gmail.com>
2025-01-05 10:26:50 +0530

    added .env to frontend

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/v1/reservation/send`,
         { firstName, lastName, email, person, date, time },
         {
           headers: {
